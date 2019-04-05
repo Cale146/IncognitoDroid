05dab9a59b23492994c5706440cb824d27263bdd
Cale <sbkrcr@gmail.com>
2019-04-04 20:50:56 -0400

    Cleaned up code a bit...

--- app/src/main/java/cale146/com/incognitodroid/CallReceiver.java
+++ app/src/main/java/cale146/com/incognitodroid/CallReceiver.java
@@ -6,33 +6,35 @@ import android.content.Intent;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
-import static cale146.com.incognitodroid.MainActivity.f808a;
+import static cale146.com.incognitodroid.MainActivity.mainActivityFlag;
 
 public class CallReceiver extends BroadcastReceiver {
-    /* renamed from: a */
-    static boolean flag = true;
+    static boolean callReceiverFlag = true;
 
     public void onReceive(Context context, Intent intent) {
         Log.d("A call been received", "true");
+
         if (!intent.getStringExtra("state").equals(TelephonyManager.EXTRA_STATE_OFFHOOK)
                 && intent.getStringExtra("state").equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+
             Log.d("Call is disconnected", "true");
-            Intent intent2;
-            if (f808a) {
-                flag = true;
-                intent2 = new Intent();
-                intent2.setClass(context, MainActivity.class);
-                intent2.putExtra("value", f808a);
-                //intent2.setFlags(Intent.);
-                context.startActivity(intent2);
+
+            Intent intentForMainActivity;
+
+            if(mainActivityFlag) {
+                callReceiverFlag = true;
+                intentForMainActivity = new Intent();
+                intentForMainActivity.setClass(context, MainActivity.class);
+                intentForMainActivity.putExtra("value", mainActivityFlag);
+                context.startActivity(intentForMainActivity);
                 return;
             }
-            flag = false;
-            intent2 = new Intent();
-            intent2.setClass(context, MainActivity.class);
-            intent2.putExtra("value", flag);
-            //intent2.setFlags(268435456);
-            context.startActivity(intent2);
+
+            callReceiverFlag = false;
+            intentForMainActivity = new Intent();
+            intentForMainActivity.setClass(context, MainActivity.class);
+            intentForMainActivity.putExtra("value", callReceiverFlag);
+            context.startActivity(intentForMainActivity);
         }
     }
 }
